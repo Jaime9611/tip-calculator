95c6dd48212192e033687cce6ed0a8827857b83d
Jaime9611 <jaimeescobar9611@gmail.com>
2021-08-21 10:07:14 -0500

    Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,17 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+var BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "script.js",
+    filename: "js/script.js",
     clean: true,
   },
   resolve: {
     extensions: [".js"],
-
   },
   module: {
     rules: [
@@ -36,7 +36,9 @@ module.exports = {
       template: "./public/index.html",
       filename: "./index.html",
     }),
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+    }),
     new CopyPlugin({
       patterns: [
         {
@@ -44,6 +46,11 @@ module.exports = {
           to: "assets/images",
         },
       ],
+    }),
+    new BrowserSyncPlugin({
+      host: "localhost",
+      port: 3000,
+      proxy: "http://localhost:8080/",
     }),
   ],
 };
