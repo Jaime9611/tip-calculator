0af5aab0545b92bb30e74ab0c3fc7d9a5d10f8e3
Jaime9611 <jaimeescobar9611@gmail.com>
2021-08-23 13:25:21 -0500

    Button Functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./styles/main.scss";
+
+const custom_tip = document.querySelector("#custom-tip");
+const custom_tip_input = document.querySelector("#tcustom");
+
+const hide_btn = (e) => {
+  custom_tip.classList.add("input-hide");
+};
+
+const show_input = (e) => {
+  custom_tip_input.classList.remove("input-hide");
+};
+
+custom_tip.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (!e.target.classList.contains("input-hide")) {
+    hide_btn();
+    show_input();
+  }
+});
