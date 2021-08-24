2cea107daaa80d2d6c9a779a3163ced1e5191f1a
Jaime9611 <jaimeescobar9611@gmail.com>
2021-08-24 13:02:07 -0500

    Basic Math stablished

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
 import "./styles/main.scss";
 import { bill_value, bill_input } from "./helpers/bill_input";
 import { persons, persons_input } from "./helpers/persons_input";
-import {
-  get_percentage,
-  isChecked,
-  tip_radio_inputs,
-} from "./helpers/percentage_fields";
+import { get_percentage, tip_radio_inputs } from "./helpers/percentage_fields";
 
-const checkValues = () => {
-  if (persons && bill_value) {
-    console.log("hi");
+const tip_total = document.querySelector("#tip-total");
+const bill_total = document.querySelector("#bill-total");
+
+const calculateValues = () => {
+  if (persons && bill_value && get_percentage()) {
+    let tip_per_person = (bill_value * get_percentage()) / persons;
+    let total_bill = bill_value + bill_value * get_percentage();
+    let total_per_person = total_bill / persons;
+    console.log(total_bill);
+
+    tip_total.innerText = `${Math.floor(tip_per_person * 100) / 100}`;
+    bill_total.innerText = `${Math.ceil(total_per_person * 100) / 100}`;
   }
 };
 
-persons_input.addEventListener("change", () => {
-  checkValues();
+persons_input.addEventListener("input", () => {
+  calculateValues();
 });
 
-bill_input.addEventListener("change", () => {
-  checkValues();
+bill_input.addEventListener("input", () => {
+  calculateValues();
+});
+
+tip_radio_inputs.forEach((input) => {
+  input.addEventListener("change", () => {
+    calculateValues();
+  });
 });
