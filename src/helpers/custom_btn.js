17c78a7ebdefa0f86c86f8cc1e0093ebdc14de8d
Jaime9611 <jaimeescobar9611@gmail.com>
2021-08-25 08:58:36 -0500

    Form Validation

--- src/helpers/custom_btn.js
+++ src/helpers/custom_btn.js
@@ -4,6 +4,7 @@ import { set_percentage, reset_percentage } from "./percentage_fields";
 // Custom Button and input
 const custom_btn = document.querySelector("#custom-tip");
 const custom_tip_input = document.querySelector("#tcustom");
+const inputError = document.querySelector("#tcustom + .input-error--tips");
 
 const hide_btn = () => {
   if (!custom_btn.classList.contains("input-hide")) {
@@ -34,15 +35,35 @@ custom_btn.addEventListener("click", (e) => {
 });
 
 custom_tip_input.addEventListener("input", () => {
-  let value = parseInt(custom_tip_input.value);
-  if (value !== NaN) {
+  if (custom_tip_input.validity.valid) {
+    inputError.innerHTML = "";
+    inputError.className = "input-error";
+    let value = parseInt(custom_tip_input.value);
     set_percentage(value);
-    calculateValues();
+  } else {
+    showError();
+    set_percentage(0);
   }
+  calculateValues();
 });
 
 const reset_custom_input = () => {
   show_btn();
+};
+
+const showError = () => {
+  if (custom_tip_input.validity.badInput) {
+    inputError.textContent = "Must be a number";
+  } else if (custom_tip_input.validity.rangeUnderflow) {
+    if (custom_tip_input.value === "0") {
+      inputError.textContent = "Can´t be zero";
+    } else {
+      inputError.textContent = "Can´t negative";
+    }
+  } else if (custom_tip_input.validity.stepMismatch) {
+    inputError.className = "input-error active";
+  }
+  inputError.className = "input-error--tips active";
 };
 
 export { show_btn, custom_tip_input, custom_btn, reset_custom_input };
